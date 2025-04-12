86727b07da5a5b1d6228bdb8fce99463f6c096e6
kelsey.makale <kelsey.makale@interswitchgroup.com>
2025-04-12 16:25:33 +0300

    A lightweight Swift library for connecting to MQTT brokers and handling seismic activity messages.

---
+++ SeismicLib/Seismic.swift
@@ -0,0 +1,8 @@
+//
+//  Seismic.swift
+//  SeismicLib
+//
+//  Created by Kelsey Makale on 12/04/2025.
+//
+
+import Foundation
---
+++ SeismicLib/SeismicActivity.swift
@@ -0,0 +1,8 @@
+//
+//  SeismicActivity.swift
+//  SeismicLib
+//
+//  Created by Kelsey Makale on 12/04/2025.
+//
+
+import Foundation
