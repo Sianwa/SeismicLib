883e96e9d30bf9b904273d0a9394d235ec9b49df
kelsey.makale <kelsey.makale@interswitchgroup.com>
2025-04-12 12:36:45 +0300

    Initial Commit

---
+++ SeismicLib/SeismicLib.h
@@ -0,0 +1,18 @@
+//
+//  SeismicLib.h
+//  SeismicLib
+//
+//  Created by Kelsey Makale on 12/04/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SeismicLib.
+FOUNDATION_EXPORT double SeismicLibVersionNumber;
+
+//! Project version string for SeismicLib.
+FOUNDATION_EXPORT const unsigned char SeismicLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SeismicLib/PublicHeader.h>
+
+
